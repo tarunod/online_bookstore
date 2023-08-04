d9c16887b4343e4ca69c5e0891f23f970946705d
tarun <tarunaod5@gecg28.ac.in>
2023-08-04 18:08:39 +0530

    fist

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,12 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.jdddds</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          className="App-lin
+          href="
+          https:target="_blank" //reactjs.org"
           rel="noopener noreferrer"
         >
           Learn React
